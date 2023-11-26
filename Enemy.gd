ce5c4ddeccf535defb2384963460f4233a037336
Kromer0 <kromka172@gmail.com>
2023-11-26 03:21:27 +0100

    szczuras asnimacjasz

--- Enemy.gd
+++ Enemy.gd
@@ -1,6 +1,13 @@
 extends CharacterBody2D
+
 func _physics_process(delta):
 	position += (get_parent().get_node("CanvasLayer/Player").position - position).normalized()
+	
+	if (velocity.x>0): #to nie ma velocity
+		get_node("AnimatedSprite2D").flip_h = true
+	else:
+		get_node("AnimatedSprite2D").flip_h = false
+
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
 		pass
@@ -9,3 +16,4 @@ func _on_area_2d_body_entered(body):
 func _on_deduwa_body_entered(body):
 	if body.name == "Player":
 		get_tree().quit()
+	
